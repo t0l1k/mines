db4a9080277bb9ed3c1e64b17eadff147cf037e6
t0l1k <t0l1k@mail.ru>
2018-10-11 20:23:58 +0300

    Statusline almost done. The Board begins.

--- main.go
+++ main.go
@@ -32,6 +32,16 @@ type (
 	StatusLine struct {
 		buttons      []buttonsData
 		btnInstances []interface{}
+		newGameBoard boardConfig
+	}
+	GameBoard struct {
+		rect          sdl.Rect
+		buttons       []Button
+		btnInstances  []interface{}
+		flagLabel     Label
+		timerLabel    Label
+		colors        []sdl.Color
+		gameBoardSize boardConfig
 	}
 	// кнопки строки статуса
 	buttonsType int
@@ -43,6 +53,10 @@ type (
 	}
 	// События
 	Event int
+	// Поле игры
+	boardConfig struct {
+		row, column, mines int32
+	}
 	// UI для sdl2
 	Label struct {
 		pos      sdl.Point
@@ -53,13 +67,13 @@ type (
 	}
 
 	Button struct {
-		rect               sdl.Rect
-		font               *ttf.Font
-		fontSize           int32
-		text               string
-		fgColor, bgColor   sdl.Color
-		focus, hide, dirty bool
-		cursor             MouseCursor
+		rect             sdl.Rect
+		font             *ttf.Font
+		fontSize         int32
+		text             string
+		fgColor, bgColor sdl.Color
+		focus, hide      bool
+		cursor           MouseCursor
 	}
 
 	Arrow struct {
@@ -80,9 +94,10 @@ const (
 	NilEvent Event = iota
 	TickEvent
 	QuitEvent
+	NewGameEvent
+	GetGameConfigEvent
 	PauseEvent
 	ResetGameEvent
-	NewGameEvent
 	IncRowEvent
 	DecRowEvent
 	IncColumnEvent
@@ -123,11 +138,11 @@ const (
 	minColumn = 5
 	maxColumn = 16
 	minMines  = 5
-	maxMines  = 99
+	maxMines  = 40
 )
 
 var (
-	mn                   int32 = 2
+	mn                   int32 = 3
 	WinWidth, WinHeight  int32 = 320 * mn, 180 * mn
 	row, column, mines   int   = 8, 8, 10
 	FontName                   = "data/Roboto-Regular.ttf"
@@ -139,25 +154,21 @@ var (
 	StatusLineHeight     int32 = WinHeight / 20
 )
 
-/*
-     ***** *                    *                  ***
-  ******  *                   **                    ***
- **   *  *                    **                     **
-*    *  *                     **                     **
-    *  *                      **                     **
-   ** **              ****    ** ****       ***      **
-   ** **             * ***  * *** ***  *   * ***     **
-   ** **            *   ****  **   ****   *   ***    **
-   ** **           **    **   **    **   **    ***   **
-   ** **           **    **   **    **   ********    **
-   *  **           **    **   **    **   *******     **
-      *            **    **   **    **   **          **
-  ****           * **    **   **    **   ****    *   **
- *  *************   ***** **   *****      *******    *** *
-*     *********      ***   **   ***        *****      ***
-*
- **
-*/
+/****** *    **           *****  *
+  ******  *  *****        ******  *
+ **   *  *     *****     **   *  *
+*    *  **     * **     *    *  *
+    *  ***     *            *  *
+   **   **     *           ** **
+   **   **     *           ** **
+   **   **     *         **** **
+   **   **     *        * *** **
+   **   **     *           ** **
+    **  **     *      **   ** **
+     ** *      *     ***   *  *
+      ***      *      ***    *
+       ********        ******
+         ****            ****/
 
 func (t *Label) New(pos sdl.Point, text string, color sdl.Color, fontSize int32) (err error) {
 	t.pos = pos
@@ -211,26 +222,6 @@ func (m MouseCursor) String() string {
 	return fmt.Sprintf("Mouse x:%v y:%v button:%v", m.X, m.Y, m.button)
 }
 
-/*
-   ***** **
-  ******  ***                     *         *
- **   *  * **                    **        **
-*    *  *  **                    **        **
-    *  *   *    **   ****      ********  ********    ****
-   ** **  *      **    ***  * ********  ********    * ***  * ***  ****
-   ** ** *       **     ****     **        **      *   ****   **** **** *
-   ** ***        **      **      **        **     **    **     **   ****
-   ** ** ***     **      **      **        **     **    **     **    **
-   ** **   ***   **      **      **        **     **    **     **    **
-   *  **     **  **      **      **        **     **    **     **    **
-      *      **  **      **      **        **     **    **     **    **
-  ****     ***    ******* **     **        **      ******      **    **
- *  ********       *****   **     **        **      ****       ***   ***
-*     ****                                                      ***   ***
-*
- **
-*/
-
 func (t *Button) New(rect sdl.Rect, text string, fgColor sdl.Color, bgColor sdl.Color, fontSize int32) (err error) {
 	t.rect = rect
 	t.fontSize = fontSize
@@ -239,7 +230,6 @@ func (t *Button) New(rect sdl.Rect, text string, fgColor sdl.Color, bgColor sdl.
 	t.bgColor = bgColor
 	t.focus = false
 	t.hide = false
-	t.dirty = true
 	err = ttf.Init()
 	if err != nil {
 		panic(err)
@@ -253,7 +243,6 @@ func (t *Button) New(rect sdl.Rect, text string, fgColor sdl.Color, bgColor sdl.
 
 func (t *Button) SetLabel(text string) {
 	t.text = text
-	t.dirty = true
 }
 
 func (b *Button) Event(event sdl.Event) int {
@@ -276,10 +265,8 @@ func (t *Button) Update() {
 	t.cursor.Update()
 	if t.cursor.InRect(&t.rect) {
 		t.focus = true
-		t.dirty = true
 	} else {
 		t.focus = false
-		t.dirty = true
 	}
 }
 
@@ -306,38 +293,17 @@ func (t *Button) paint(renderer *sdl.Renderer, fg, bg sdl.Color) (err error) {
 }
 
 func (t *Button) Render(renderer *sdl.Renderer) (err error) {
-	if t.dirty {
-		if t.focus {
-			t.paint(renderer, t.fgColor, t.bgColor)
-		} else {
-			t.paint(renderer, t.bgColor, t.fgColor)
-		}
+	if t.focus {
+		t.paint(renderer, t.fgColor, t.bgColor)
+	} else {
+		t.paint(renderer, t.bgColor, t.fgColor)
 	}
-	t.dirty = false
 	return nil
 }
 
 func (t *Button) Quit() {
 	t.font.Close()
 }
-
-/* **
-     *****
-    *  ***                                             **
-       ***                                             **
-      *  **       ***  ****    ***  ****       ****     **    ***    ****        ****
-      *  **        **** **** *  **** **** *   * ***  *   **    ***     ***  *   * **** *
-     *    **        **   ****    **   ****   *   ****    **     ***     ****   **  ****
-     *    **        **           **         **    **     **      **      **   ****
-    *      **       **           **         **    **     **      **      **     ***
-    *********       **           **         **    **     **      **      **       ***
-   *        **      **           **         **    **     **      **      **         ***
-   *        **      **           **         **    **     **      **      *     ****  **
-  *****      **     ***          ***         ******       ******* *******     * **** *
- *   ****    ** *    ***          ***         ****         *****   *****         ****
-*     **      **
-*
- ** */
 
 func (t *Arrow) New(rect sdl.Rect, text string, fgColor, bgColor sdl.Color, fontSize int32) (err error) {
 	t.rect = rect
@@ -382,6 +348,23 @@ func (t *Arrow) GetLabel() string {
 	return t.text
 }
 
+func (s *Arrow) GetNumber() (value int) {
+	arr := strings.Split(s.GetLabel(), ":")
+	num := arr[1]
+	value, err := strconv.Atoi(num)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+func (s *Arrow) SetNumber(value int) {
+	arr := strings.Split(s.GetLabel(), ":")
+	text := arr[0]
+	text += ":" + strconv.Itoa(value)
+	s.SetLabel(text)
+}
+
 func (s *Arrow) Event(event sdl.Event) (e Event, err error) {
 	for idx, button := range s.btnInstances {
 		switch event.(type) {
@@ -392,10 +375,8 @@ func (s *Arrow) Event(event sdl.Event) (e Event, err error) {
 					for i := 0; i < len(s.buttons[idx].event); i++ {
 						switch s.buttons[idx].event[i] {
 						case DecButtonEvent:
-							// fmt.Println("DecButtonEvent!", button)
 							return DecButtonEvent, nil
 						case IncButtonEvent:
-							// fmt.Println("IncButtonEvent!", button)
 							return IncButtonEvent, nil
 						}
 					}
@@ -433,24 +414,122 @@ func (s *Arrow) Render(renderer *sdl.Renderer) (err error) {
 	return nil
 }
 
-/********
-    *       ***      *                    *
-   *         **     **                   **
-   **        *      **                   **
-    ***           ********             ******** **   ****        ****
-   ** ***        ********     ****    ********   **    ***  *   * **** *
-    *** ***         **       * ***  *    **      **     ****   **  ****
-      *** ***       **      *   ****     **      **      **   ****
-        *** ***     **     **    **      **      **      **     ***
-          ** ***    **     **    **      **      **      **       ***
-           ** **    **     **    **      **      **      **         ***
-            * *     **     **    **      **      **      **    ****  **
-  ***        *      **     **    **      **       ******* **  * **** *
- *  *********        **     ***** **      **       *****   **    ****
-*     *****                  ***   **
+/****** **                                           **
+  ******  ***                                            **
+ **   *  * **                                            **
+*    *  *  **                                            **
+    *  *   *       ****               ***  ****          **
+   ** **  *       * ***  *    ****     **** **** *   *** **
+   ** ** *       *   ****    * ***  *   **   ****   *********
+   ** ***       **    **    *   ****    **         **   ****
+   ** ** ***    **    **   **    **     **         **    **
+   ** **   ***  **    **   **    **     **         **    **
+   *  **     ** **    **   **    **     **         **    **
+      *      ** **    **   **    **     **         **    **
+  ****     ***   ******    **    **     ***        **    **
+ *  ********      ****      ***** **     ***        *****
+*     ****                   ***   **                ***
 *
  ***/
+func (s *GameBoard) New(b boardConfig) (err error) {
+	s.gameBoardSize = boardConfig{b.row, b.column, b.mines}
+	s.Setup()
+	return nil
+}
+
+func (s *GameBoard) Setup() (err error) {
+	var x, y, w, h, dx, dy, boardColumn int32
+	s.colors = []sdl.Color{sdl.Color{32, 32, 0, 0}, sdl.Color{255, 255, 0, 0}}
+	w, h = int32(float64(WinHeight)/1.1), int32(float64(WinHeight)/1.1)
+	x, y = (WinHeight-w)/2, (WinHeight-h)/2+StatusLineHeight/2
+	s.rect = sdl.Rect{x, y, w, h}
+	if s.gameBoardSize.row > s.gameBoardSize.column {
+		boardColumn = s.gameBoardSize.row
+	} else {
+		boardColumn = s.gameBoardSize.column
+	}
+	cellWidth, cellHeight := w/boardColumn, (h-StatusLineHeight*2)/boardColumn
+	cellFontSize := cellHeight - 3
+	if len(s.btnInstances) > 0 {
+		s.btnInstances = nil
+	}
+	for dy = 0; dy < s.gameBoardSize.column; dy++ {
+		for dx = 0; dx < s.gameBoardSize.row; dx++ {
+			x = s.rect.X + dx*cellWidth
+			y = s.rect.Y + dy*cellHeight
+			w = cellWidth
+			h = cellHeight
+			b := &Button{}
+			if err := b.New(sdl.Rect{x, y, w, h}, strconv.Itoa(int(dy*s.gameBoardSize.row+dx)), s.colors[1], s.colors[0], cellFontSize); err != nil {
+				panic(err)
+			}
+			s.btnInstances = append(s.btnInstances, b)
+		}
+	}
+	arr := []string{"0/0", "000"}
+	for dx = 0; dx < int32(len(arr)); dx++ {
+		w = (s.rect.H / int32((len(arr) + 1)))
+		x = s.rect.X + dx*w + w
+		y = s.rect.H - StatusLineHeight/2
+		l := &Label{}
+		if err = l.New(sdl.Point{x, y}, arr[dx], s.colors[1], StatusLineFontSize); err != nil {
+			panic(err)
+		}
+		s.btnInstances = append(s.btnInstances, l)
+	}
+	return nil
+}
+
+func (s *GameBoard) Update(event Event) error {
+	for idx, button := range s.btnInstances {
+		switch button.(type) {
+		case *Button:
+			s.btnInstances[idx].(*Button).Update()
+		case *Label:
+		}
+	}
+	return nil
+}
+func (s *GameBoard) Render(renderer *sdl.Renderer) (err error) {
+	renderer.SetDrawColor(s.colors[0].R, s.colors[0].G, s.colors[0].B, s.colors[0].A)
+	renderer.DrawRect(&s.rect)
+	for _, button := range s.btnInstances {
+		switch button.(type) {
+		case *Button:
+			if err = button.(*Button).Render(renderer); err != nil {
+				panic(err)
+			}
+		case *Label:
+			if err = button.(*Label).Render(renderer); err != nil {
+				panic(err)
+			}
+		}
+	}
+	return nil
+}
+func (s *GameBoard) Event(event sdl.Event) (e Event, err error) {
+	return NilEvent, nil
+}
+
+/********                                                                 ***** *
+    *       ***      *                    *                                ******  *
+   *         **     **                   **                               **   *  *
+   **        *      **                   **                              *    *  *
+    ***           ********             ******** **   ****        ****        *  *
+   ** ***        ********     ****    ********   **    ***  *   * **** *    ** **           ***  ****
+    *** ***         **       * ***  *    **      **     ****   **  ****     ** **            **** **** *
+      *** ***       **      *   ****     **      **      **   ****          ** **             **   ****
+        *** ***     **     **    **      **      **      **     ***         ** **             **    **
+          ** ***    **     **    **      **      **      **       ***       ** **             **    **
+           ** **    **     **    **      **      **      **         ***     *  **             **    **
+            * *     **     **    **      **      **      **    ****  **        *              **    **
+  ***        *      **     **    **      **       ******* **  * **** *     ****           *   **    **
+ *  *********        **     ***** **      **       *****   **    ****     *  *************    ***   ***
+*     *****                  ***   **                                    *     *********       ***   ***
+*                                                                        *
+ **                                                                       ***/
 func (s *StatusLine) Setup() (err error) {
+	s.newGameBoard = boardConfig{8, 8, 10}
 	s.buttons = []buttonsData{
 		{name: buttonQuit, rect: sdl.Rect{0, 0, StatusLineHeight, StatusLineHeight}, text: "<-", event: []Event{QuitEvent}},
 		{name: buttonPause, rect: sdl.Rect{StatusLineHeight, 0, StatusLineHeight * 3, StatusLineHeight}, text: "Pause", event: []Event{PauseEvent}},
@@ -508,13 +587,35 @@ func (s *StatusLine) Setup() (err error) {
 	return nil
 }
 
+func (s *StatusLine) GetGameBoardSize() boardConfig {
+	return s.newGameBoard
+}
+
 func (s *StatusLine) Update(event Event) error {
-	for i, button := range s.btnInstances {
+	switch event {
+	case NewGameEvent:
+		fmt.Printf("start new game:%v", s.newGameBoard)
+	case IncRowEvent: // Replace game board size by arrows
+		s.newGameBoard.row = int32(s.btnInstances[4].(*Arrow).GetNumber())
+	case DecRowEvent:
+		s.newGameBoard.row = int32(s.btnInstances[4].(*Arrow).GetNumber())
+	case IncColumnEvent:
+		s.newGameBoard.column = int32(s.btnInstances[5].(*Arrow).GetNumber())
+	case DecColumnEvent:
+		s.newGameBoard.column = int32(s.btnInstances[5].(*Arrow).GetNumber())
+	case IncMinesEvent:
+		s.newGameBoard.mines = int32(s.btnInstances[6].(*Arrow).GetNumber())
+	case DecMinesEvent:
+		s.newGameBoard.mines = int32(s.btnInstances[6].(*Arrow).GetNumber())
+	}
+	for idx, button := range s.btnInstances {
 		switch button.(type) {
 		case *Button:
-			s.btnInstances[i].(*Button).Update()
+			s.btnInstances[idx].(*Button).Update()
+			if s.buttons[idx].name == buttonNew {
+			}
 		case *Arrow:
-			s.btnInstances[i].(*Arrow).Update(event)
+			s.btnInstances[idx].(*Arrow).Update(event)
 		}
 	}
 	return nil
@@ -536,7 +637,6 @@ func (s *StatusLine) Render(renderer *sdl.Renderer) (err error) {
 	}
 	return nil
 }
-
 func (s *StatusLine) Event(event sdl.Event) (e Event, err error) {
 	for idx, button := range s.btnInstances {
 		switch event.(type) {
@@ -568,34 +668,27 @@ func (s *StatusLine) Event(event sdl.Event) (e Event, err error) {
 					case buttonRow:
 						switch ev {
 						case IncButtonEvent:
-							fmt.Println("Get inc row event", s.buttons[idx].name, ev)
 							s.calc(s.buttons[idx].name, s.btnInstances[idx].(*Arrow), "inc")
 							return IncRowEvent, nil
 						case DecButtonEvent:
-							fmt.Println("Get dec row event", s.buttons[idx].name, ev)
 							s.calc(s.buttons[idx].name, s.btnInstances[idx].(*Arrow), "dec")
 							return DecRowEvent, nil
 						}
 					case buttonCol:
 						switch ev {
 						case IncButtonEvent:
-							fmt.Println("Get inc column event", s.buttons[idx].name, ev)
 							s.calc(s.buttons[idx].name, s.btnInstances[idx].(*Arrow), "inc")
 							return IncColumnEvent, nil
 						case DecButtonEvent:
-							fmt.Println("Get dec column event", s.buttons[idx].name, ev)
 							s.calc(s.buttons[idx].name, s.btnInstances[idx].(*Arrow), "dec")
 							return DecColumnEvent, nil
-
 						}
 					case buttonMines:
 						switch ev {
 						case IncButtonEvent:
-							fmt.Println("Get inc mines event", s.buttons[idx].name, ev)
 							s.calc(s.buttons[idx].name, s.btnInstances[idx].(*Arrow), "inc")
 							return IncMinesEvent, nil
 						case DecButtonEvent:
-							fmt.Println("Get dec mines event", s.buttons[idx].name, ev)
 							s.calc(s.buttons[idx].name, s.btnInstances[idx].(*Arrow), "dec")
 							return DecMinesEvent, nil
 						}
@@ -608,29 +701,40 @@ func (s *StatusLine) Event(event sdl.Event) (e Event, err error) {
 }
 
 func (s *StatusLine) calc(name buttonsType, instance *Arrow, op string) {
-	text := instance.GetLabel()
-	textArr := strings.Split(text, ":")
-	text, num := textArr[0], textArr[1]
-	n, err := strconv.Atoi(num)
-	if err != nil {
-		panic(err)
-	}
-	if name == buttonRow || name == buttonCol || name == buttonMines {
-
-		if n < maxMines {
-			if op == "inc" {
+	n := instance.GetNumber()
+	switch op {
+	case "inc":
+		switch name {
+		case buttonRow:
+			if n < maxRow {
+				n++
+			}
+		case buttonCol:
+			if n < maxColumn {
+				n++
+			}
+		case buttonMines:
+			if n < maxMines {
 				n++
 			}
 		}
-		if n > minMines {
-			if op == "dec" {
+	case "dec":
+		switch name {
+		case buttonRow:
+			if n > minRow {
+				n--
+			}
+		case buttonCol:
+			if n > minColumn {
+				n--
+			}
+		case buttonMines:
+			if n > minMines {
 				n--
 			}
 		}
 	}
-	text += ":" + strconv.Itoa(n)
-	fmt.Println("update", text, textArr, num, n, op, name)
-	instance.SetLabel(text)
+	instance.SetNumber(n)
 }
 
 /*    *****   **    **
@@ -703,7 +807,7 @@ func (s *View) Setup() (err error) {
 		panic(err)
 	}
 	sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
-	s.pushTime = 1
+	s.pushTime = 100
 	s.lastPushTime = sdl.GetTicks()
 	return nil
 }
@@ -771,17 +875,36 @@ func (s *Spinner) Run(m Mines, v View) {
 	if err := v.Setup(); err != nil {
 		panic(err)
 	}
-	st := &StatusLine{}
-	st.Setup()
-	s.mines.Attach(st)
+	statusLine := &StatusLine{}
+	statusLine.Setup()
+	s.mines.Attach(statusLine)
+	board := &GameBoard{}
+	board.New(boardConfig{8, 8, 10})
+	s.mines.Attach(board)
 	dirty := true
 	running := true
 	for running {
 		for _, event := range v.GetEvents(s.mines.GetSubscribers()) {
-			if event == QuitEvent {
+			switch event {
+			case QuitEvent:
 				running = false
-			} else if event == TickEvent {
+			case TickEvent:
 				dirty = true
+			case NewGameEvent:
+				fmt.Printf("start new game:%v", statusLine.newGameBoard)
+				board.New(statusLine.newGameBoard)
+			case IncRowEvent: // Replace game board size by arrows
+				statusLine.newGameBoard.row = int32(statusLine.btnInstances[4].(*Arrow).GetNumber())
+			case DecRowEvent:
+				statusLine.newGameBoard.row = int32(statusLine.btnInstances[4].(*Arrow).GetNumber())
+			case IncColumnEvent:
+				statusLine.newGameBoard.column = int32(statusLine.btnInstances[5].(*Arrow).GetNumber())
+			case DecColumnEvent:
+				statusLine.newGameBoard.column = int32(statusLine.btnInstances[5].(*Arrow).GetNumber())
+			case IncMinesEvent:
+				statusLine.newGameBoard.mines = int32(statusLine.btnInstances[6].(*Arrow).GetNumber())
+			case DecMinesEvent:
+				statusLine.newGameBoard.mines = int32(statusLine.btnInstances[6].(*Arrow).GetNumber())
 			}
 			s.mines.Notify(event)
 		}
